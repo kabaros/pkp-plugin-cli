e41a5649518cb48f3e99a93f81f1b9d56c2ca84b
mozafar <kabaros@gmail.com>
2020-04-25 22:15:45 +0100

    Update files headers and documentation

--- src/release/index.js
+++ src/release/index.js
@@ -1,3 +1,14 @@
+/**
+ * @file src/release/index.js
+ *
+ * Copyright (c) 2020 Simon Fraser University
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @brief Entry point for release command
+ *
+ * This command (pkp-plugin release) is to be implemented
+ */
+
 const { warn, info } = require('../utils/log')
 const publishRelease = require('./publishRelease')
 
